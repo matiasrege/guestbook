c7d92ea913ff2f36c26331ced9179807573b8ae1
Jonathan Leibiusky <jonathan.leibiusky@mercadolibre.com>
2010-05-11 17:38:30 -0300

    Agregado de delete de un mensaje

--- grails-app/controllers/guestbook/MensajeController.groovy
+++ grails-app/controllers/guestbook/MensajeController.groovy
@@ -1,6 +1,11 @@
 package guestbook
 
 class MensajeController {
+    def delete = {
+        def m = Mensaje.load(param.id)
+        m.delete()
+        redirect action:"index"
+    }
     def create = {
         def m = new Mensaje(nombreUsuario:params.nombre, texto:params.mensaje)
         m.save()
